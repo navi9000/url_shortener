34889696498f70383b610f80227ac86cc0953737
Ivan Tsapko <icdi7434@gmail.com>
2021-11-21 18:42:47 +0300

    deleted sandbox

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\URLShortenerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/sandbox', function () {
-    return view('sandbox');
-});
-
 Route::get('/', [URLShortenerController::class, 'index'])->name('main');
 Route::post('/', [URLShortenerController::class, 'generate']);
 Route::get('/{token}', [URLShortenerController::class, 'remote'])->name('remote');
