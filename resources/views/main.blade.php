0126045e2f103a51f3029f0fa35c50550141cd54
Ivan Tsapko <icdi7434@gmail.com>
2022-03-04 14:34:28 +0300

    changed to secure_asset

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 <div class="max-w-5xl bg-white rounded-lg my-6 mx-auto p-12 shadow-md">
-    <form method="post" action="{{secure_url('/')}}" class="flex gap-6">
+    <form method="post" action="{{secure_asset('main')}}" class="flex gap-6">
         @csrf
         <input type="text" autocomplete="off" placeholder="Введите URL" name="url" class="flex-1 focus:outline-none focus:ring-2 focus:ring-red-600 rounded-lg p-3">
         <button type="submit" class="flex-0 ring-red-600 ring-2 rounded-lg p-3 text-red-600 font-bold hover:bg-red-600 hover:text-white hover:shadow-lg transition duration-300 ease-out">Получить короткую ссылку</button>
     </form>
     @if (session('token'))
-    <form action="{{secure_url('/', ['token' => session('token')])}}">
+    <form action="{{secure_asset('remote', ['token' => session('token')])}}">
         @csrf
 
-        <button type="submit" class="my-6 hover:underline">{{ltrim(url()->current(), 'https://') . '/' . session('token')}}</button>
+        <button type="submit" class="my-6 hover:underline">{{ltrim(url()->current(), 'http://') . '/' . session('token')}}</button>
     </form>
     @endif
 </div>
