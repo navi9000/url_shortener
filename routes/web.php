e18747415184edccf8977535b181c3b8193a7b43
Ivan Tsapko <icdi7434@gmail.com>
2022-03-04 13:30:58 +0300

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\URLShortenerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [URLShortenerController::class, 'index'])->name('main');
-Route::post('/', [URLShortenerController::class, 'generate'])->name('generate');
+Route::post('/', [URLShortenerController::class, 'generate']);
 Route::get('/{token}', [URLShortenerController::class, 'remote'])->name('remote');
